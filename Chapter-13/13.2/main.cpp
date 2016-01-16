9e279a07df3566f13166a88b4c6caa06399720a7
phn10 <phn.sci@gmail.com>
2016-01-16 18:33:41 -0500

    Create main.cpp

---
+++ Chapter-13/13.2/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Employee.h"
+
+int main()
+{
+	const int NUM_EMP = 3;
+	Employee employee[NUM_EMP] = 
+	{ Employee("Susan Meyers", 47899, "Accounting   ", "Vice President"),
+	  Employee("Mark Jones  ", 39119, "IT           ", "Programmer    "),
+	  Employee("Joy Rogers  ", 81774, "Manufacturing", "Engineer      ") };
+	
+	std::cout << "Name                  "
+		      << "ID                "
+		      << "Department              " 
+		      << "Position       " << std::endl;
+	for (int count = 0; count < NUM_EMP; count++)
+	{
+		std::cout << employee[count].getName()
+				  << std::setw(15) << employee[count].getID()
+				  << "\t\t" << employee[count].getDepart()
+				  << "\t\t" << employee[count].getPosi() << std::endl;
+	}
+	std::cin.get();
+	std::cin.get();
+	return 0;
+}
