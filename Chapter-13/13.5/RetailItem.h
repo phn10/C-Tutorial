6e992959829f912d8ffba4efb5a53ea3e9deac77
phn10 <phn.sci@gmail.com>
2016-01-17 21:19:09 -0500

    Create RetailItem.h

---
+++ Chapter-13/13.5/RetailItem.h
@@ -0,0 +1,27 @@
+#ifndef RETAILITEM_H
+#define RETAILITEM_H
+#include <string>
+
+class RetailItem
+{
+	private:
+		std::string desc;
+		int units;
+		double price;
+	public:
+		RetailItem(std::string d, int u, double p)
+		{ desc = d; units = u; price = p; }  
+		void setDescription(std::string d)
+		{ desc = d; }
+		void setUnits(int u)
+		{ units = u; }
+		void setPrice(double p)
+		{ price = p; }
+		std::string getDescription()
+		{ return desc; }
+		int getUnits()
+		{ return units; }
+		double getPrice()
+		{ return price; }
+};
+#endif
