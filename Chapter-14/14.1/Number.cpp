118b299761718302ead2671cb0e26dcf7c9f22da
phn10 <phn.sci@gmail.com>
2016-01-19 21:23:13 -0500

    Update Number.cpp

--- Chapter-14/14.1/Number.cpp
+++ Chapter-14/14.1/Number.cpp
@@ -1,60 +1,10 @@
-#ifndef NUMBER_H
-#define NUMBER_H
+#include "Number.h"
 #include <string>
-#include <stdlib.h>
-#include <iostream>
-
 using namespace std;
 
-class Number
-{
-	private:
-		int u;
-		char number[4]; 
-		static string lessThan20[20];
-		static string ten[10];
-	public:
-		Number(char num[], int c)
-		{
-			for (int i = 0; i < c; i++)
-				number[i] = num[i];
-		};
-			
-
-		string printString()
-		{
-			string print = "";
-			if ((number[0] - '0') > 0)
-			{
-				print.append(lessThan20[(number[0] - '0')]);
-				print.append(" thousand ");
-			}
-			if ((number[1] - '0') > 0)
-			{
-				print.append(lessThan20[(number[1] - '0')]);
-				print.append(" hundred and ");
-			}
-			if ((number[2] - '0') > 0)
-			{
-				if ((number[2] - '0') > 1)
-				{
-					print.append(ten[number[2] - '0']);
-					print.append(" ");
-					print.append(lessThan20[(number[3] - '0')]);
-					return print;
-				}
-				else if ((number[2] - '0') == 1)
-				{
-					char x = number[2];
-					char y = number[3];
-					int num = atoi(&x) * 10 + atoi(&y);
-					print.append(lessThan20[num]);
-				}
-			}
-			if ((number[3] - '0') >= 0)
-				print.append(lessThan20[(number[3] - '0')]);
-				
-			return print;
-		};			
-};
-#endif
+string Number::lessThan20[20] = {"zero", "one", "two", "three", "four", "five",
+							  "six", "seven", "eight", "nine", "ten", "eleven", 
+						   	  "twelve", "thirteen", "fourteen", "fifteen", "sixteen", 
+							  "seventeen", "eighteen", "nineteen"};
+string Number::ten[10] = {"zero", "ten", "twenty", "thirty", "forty", "fifty", 
+				       "sixty", "seventy", "eighty", "ninety"};
