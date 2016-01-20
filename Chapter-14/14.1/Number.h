3a64490365a3df9ab9af4e655efd1162e466040e
phn10 <phn.sci@gmail.com>
2016-01-20 13:46:32 -0500

    Update Number.h

--- Chapter-14/14.1/Number.h
+++ Chapter-14/14.1/Number.h
@@ -10,14 +10,18 @@ class Number
 {
 	private:
 		int u;
-		char number[4]; 
+		static char number[4]; 
 		static string lessThan20[20];
 		static string ten[10];
 	public:
 		Number(char num[], int c)
 		{
-			for (int i = 0; i < c; i++)
-				number[i] = num[i];
+			for (int i = c - 1; i >= 0; i--)
+				number[i + 4 - c] = num[i];
+
+			if (c != 4)
+				for (int i = 0; i < 4 - c; i++)
+					number[i] = '0';
 		};
 			
 
@@ -34,26 +38,24 @@ class Number
 				print.append(lessThan20[(number[1] - '0')]);
 				print.append(" hundred and ");
 			}
-			if ((number[2] - '0') > 0)
+
+			if ((number[2] - '0') > 1)
 			{
-				if ((number[2] - '0') > 1)
-				{
-					print.append(ten[number[2] - '0']);
-					print.append(" ");
-					print.append(lessThan20[(number[3] - '0')]);
-					return print;
-				}
-				else if ((number[2] - '0') == 1)
-				{
-					char x = number[2];
-					char y = number[3];
-					int num = atoi(&x) * 10 + atoi(&y);
-					print.append(lessThan20[num]);
-				}
-			}
-			if ((number[3] - '0') >= 0)
+				print.append(ten[number[2] - '0']);
+				print.append(" ");
 				print.append(lessThan20[(number[3] - '0')]);
-				
+				return print;
+			}
+			else if ((number[2] - '0') == 1)
+			{
+				char x = number[2];
+				char y = number[3];
+				int num = atoi(&x) * 10 + atoi(&y);
+				print.append(lessThan20[num]);
+			}
+			else if ((number[2] - '0') == 0)
+				print.append(lessThan20[(number[3] - '0')]);
+
 			return print;
 		};			
 };
