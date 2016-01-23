a372b8d886e335e338fe1950ea3c9e2ff079f91c
phn10 <phn.sci@gmail.com>
2016-01-23 16:46:37 -0500

    Create NumberList.h

---
+++ Chapter-17/17.1/NumberList.h
@@ -0,0 +1,27 @@
+#ifndef NUMBERLIST
+#define NUMBRELIST
+
+class NumberList
+{
+	private:
+
+		// declare a struct for list
+		struct node
+		{
+			int value;    // value store in this struct
+			node *next;   // next pointer point to next struct
+		};
+		node *head; // beginning list head pointer
+	public:
+		
+		// constructor 
+		NumberList()
+		{ head = nullptr; }
+
+		~NumberList();
+		void appendNode(int );
+		void insertNode(int );
+		void deleteNode(int );
+		void displayList() const;
+};
+#endif
